9643b7a37a870826957b520054ffe0ed7a3cbe1d
KelvinKenny <111447723+KelvinKenny@users.noreply.github.com>
2022-09-28 20:02:44 -0300

    Add files via upload

---
+++ teste,.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use("seaborn")
+
+df = pd.read_excel(r"C:\Users\claudio.junior\Desktop\projetos\projeto 0\Testes py\AdventureWorks.xlsx")
+df.head()
